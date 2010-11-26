9b44b3d003b64572a4b60b0d3cb051915d05329c
michal <michal@machine.(none)>
2010-11-26 12:12:36 +0100

    Neuron, NeuronsLayer and NeuralNetwork inherits from vector

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -7,22 +7,21 @@ class IncorrectArguments{
 };
 
 template <class T>
-struct Neuron{
+struct Neuron : public vector<T>{
     T output;
-    vector<T> * weights;
     
     Neuron() : output(0){}
     
     void setSize(int size){
-	weights = new vector<T>(size);
+	this->resize(size);
 	for(int i=0; i<size; ++i)
-	    weights->at(i) = 2;//random();
+	    this->at(i) = 2;//random();
     }
     
     void computeOutput(vector<Neuron> & inputs){
-	for(int i=0, sz=weights->size(); i<sz; ++i)
-	    output += weights->at(i)*inputs[i].output;
-	output = sigmoidal(output);
+	for(int i=0, sz=this->size(); i<sz; ++i)
+	    output += this->at(i) * inputs[i].output;
+	output = Neuron<T>::sigmoidal(output);
     }
     
     T static sigmoidal(T val){
@@ -32,60 +31,50 @@ struct Neuron{
 };
 
 template <class T>
-struct NeuronsLayer{
-    vector<Neuron<T> > * neurons;
+struct NeuronsLayer : public vector<Neuron<T> >{
     void setSize(int size, int prevSize){
-	neurons = new vector<Neuron<T> >(size); //it creates vector with the size of size. For each of them default constructor of Neuron is called.
-	for(int i=0; i<neurons->size(); ++i)
-	    neurons->at(i).setSize(prevSize);
+	this->resize(size);
+	for(int i=0; i<this->size(); ++i)
+	    this->at(i).setSize(prevSize);
     }
-    void computeOutputs(NeuronsLayer & prev){
-	for(int i=0, sz=neurons->size(); i<sz; ++i)
-	    neurons->at(i).computeOutput(*prev.neurons);
-    }
-    ~NeuronsLayer(){
-	delete neurons;
+    void computeOutputs(NeuronsLayer<T> & prev){
+	for(int i=0, sz=this->size(); i<sz; ++i)
+	    this->at(i).computeOutput(prev);
     }
 };
 
 template <class T>
-struct NeuralNetwork{
-    vector<NeuronsLayer<T> > * layers;
-    
+struct NeuralNetwork : public vector<NeuronsLayer<T> >{
     //First parameter specifies how many layers network will contain. Second is vector with sizes of each layer (so vector size must equal layersNumber)
     NeuralNetwork(int layersNumber, vector<int> sizes) throw(IncorrectArguments){
 	if(layersNumber!=sizes.size())
 	    throw IncorrectArguments();
-	layers = new vector<NeuronsLayer<T> >(layersNumber); //it creates vector with the size of layersNumber. for each of them default constructor of NeuralsLayer is called (it does nothing interesting)
+	this->resize(layersNumber);
 	for(int i=0; i<layersNumber; ++i)
-	    layers->at(i).setSize(sizes[i], (i==0) ? 1 : sizes[i-1]);
+	    this->at(i).setSize(sizes[i], (i==0) ? 1 : sizes[i-1]);
 	    
-    }
-    
-    ~NeuralNetwork(){
-	delete layers;
     }
     
     //set outputs of first layer neurons (first layer outputs are really inputs when we constider the whole NeuralNetwork consisted of a few layers.
     //size of inputs must match size of first layer
     void setInputValues(vector<T> & inputs) throw(IncorrectArguments){
-	if(inputs.size() != layers->at(0).neurons->size())
+	if(inputs.size() != this->at(0).size())
 	    throw IncorrectArguments();
-	for(int i=0, sz=layers->at(0).neurons->size(); i<sz; ++i){
-	    layers->at(0).neurons->at(i).output = inputs[i];
-	    layers->at(0).neurons->at(0).weights->at(0) = 1; //it's the only weight of first layer neurons
+	for(int i=0, sz=this->at(0).size(); i<sz; ++i){
+	    this->at(0).at(i).output = inputs[i]; //set 0 layer, i-th neuron's output
+	    this->at(0).at(i).at(0) = 1; //it's the only weight of first layer neurons
 	}
     }
     
     void compute(){
-	for(int i=1; i<layers->size(); ++i)
-	    layers->at(i).computeOutputs(layers->at(i-1));
+	for(int i=1; i<this->size(); ++i)
+	    this->at(i).computeOutputs(this->at(i-1));
     }
     
     void printResult(){
-	NeuronsLayer<T> & lastLayer = layers->at(layers->size()-1);
-	for(int i=0, sz=lastLayer.neurons->size(); i<sz; ++i)
-	    cout << lastLayer.neurons->at(i).output << endl;
+	NeuronsLayer<T> & lastLayer = this->at(this->size()-1);
+	for(int i=0, sz=lastLayer.size(); i<sz; ++i)
+	    cout << lastLayer.at(i).output << endl;
     }
 };
 
